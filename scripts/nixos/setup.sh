fd10883aedb935b2853765c5902130355c5912f6
ch-script <christophermoravarela@gmail.com>
2026-08-19 13:25:25 -0600

    refactor: no standard name

--- scripts/nixos/setup.sh
+++ scripts/nixos/setup.sh
@@ -6,7 +6,7 @@ echo "[ch] Setting up declarative environment in $CONFIG_FILE"
 if grep -q "\./packages.nix" "$CONFIG_FILE"; then
     echo "[ch] ./packages.nix is already in your imports. All good!!"
 else
-    echo "[ch] Creating backup of $CONFIG_FILE..."
+    echo "[ch] Creating backup of $CONFIG_FILE"
     ch --backup pkgs "$CONFIG_FILE"
     sudo sed -i '/imports =/,/]/ s/^\(\s*\)]/\1  .\/packages.nix\n\1]/' "$CONFIG_FILE"
     echo "[ch] ./packages.nix injected successfully."
